afa3aa6a8b60527dadda9ad0b1ca71011d9bc26f
Jeroen Verschaeren <jeroen.verschaeren@bignited.be>
2024-07-11 14:35:13 +0200

    add ESLint Stylistic

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@
 
 // Import the ESLint plugin
 import eslintPluginBstrict from 'eslint-plugin-bstrict/flat';
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
   {
@@ -12,19 +13,22 @@ export default [
       ecmaVersion: 'latest',
     },
     // Using the eslint-plugin-bstrict plugin defined locally
-    plugins: { 'bstrict': eslintPluginBstrict },
+    plugins: { 
+      'bstrict': eslintPluginBstrict,
+      '@stylistic/js': stylisticJs
+    },
     rules: {
       // Using the ESLint recommended rules by b.ignited
-      indent: ['error', 2],
+      '@stylistic/js/indent': ['error', 2],
       complexity: ['error', 2],
       'max-depth': ['error', 2],
       'max-lines': 'error',
       'no-irregular-whitespace': 'error',
       'no-prototype-builtins': 'off',
-      'no-multi-spaces': 'error',
+      '@stylistic/js/no-multi-spaces': 'error',
       'prefer-const': 'off',
-      'space-before-function-paren': 'off',
-      quotes: ['error', 'single', { avoidEscape: true }],
+      '@stylistic/js/space-before-function-paren': 'off',
+      '@stylistic/js/quotes': ['error', 'single', { avoidEscape: true }],
       'arrow-body-style': ['error', 'always'],
       '@typescript-eslint/no-explicit-any': 'off',
 
