0d888c931ac01d6829cafd3fb3ffd5952488258f
Lucas Stocker <lucas.stocker@bignited.be>
2025-09-09 09:21:41 +0200

    added no print rule

---
+++ src/lib/rules/no-print.ts
@@ -0,0 +1,105 @@
+import { ESLintUtils, TSESLint, TSESTree } from "@typescript-eslint/utils";
+import { RuleListener } from "@typescript-eslint/utils/ts-eslint";
+
+/**
+ * @fileoverview A rule to enforce no print statement calls.
+ * @author b.ignited
+ */
+
+const prohibitedConsoleMethods = new Set(["log", "info", "warn", "error"]);
+
+/**
+ * Reports if the call expression is a disallowed `console` method
+ * (e.g., console.log, console.info, console.warn, console.error).
+ */
+function reportIfConsoleCall(
+  node: TSESTree.CallExpression,
+  context: TSESLint.RuleContext<"noPrint", []>
+) {
+  if (
+    node.callee.type === "MemberExpression" &&
+    node.callee.object.type === "Identifier" &&
+    node.callee.object.name === "console" &&
+    node.callee.property.type === "Identifier" &&
+    prohibitedConsoleMethods.has(node.callee.property.name)
+  ) {
+    context.report({
+      node,
+      messageId: "noPrint",
+      data: {
+        name: `console.${node.callee.property.name}`,
+      },
+    });
+  }
+}
+
+/**
+ * Reports if the call expression is a call to the global `alert()` function.
+ */
+function reportIfAlertCall(
+  node: TSESTree.CallExpression,
+  context: TSESLint.RuleContext<"noPrint", []>
+) {
+  if (node.callee.type === "Identifier" && node.callee.name === "alert") {
+    context.report({
+      node,
+      messageId: "noPrint",
+      data: {
+        name: "alert",
+      },
+    });
+  }
+}
+
+/**
+ * Reports if the call expression is a call to `document.write()`.
+ */
+function reportIfDocumentWriteCall(
+  node: TSESTree.CallExpression,
+  context: TSESLint.RuleContext<"noPrint", []>
+) {
+  if (
+    node.callee.type === "MemberExpression" &&
+    node.callee.object.type === "Identifier" &&
+    node.callee.object.name === "document" &&
+    node.callee.property.type === "Identifier" &&
+    node.callee.property.name === "write"
+  ) {
+    context.report({
+      node,
+      messageId: "noPrint",
+      data: {
+        name: "document.write",
+      },
+    });
+  }
+}
+
+const createRule = ESLintUtils.RuleCreator((name) => name);
+
+const rule = createRule({
+  name: "noPrint",
+  meta: {
+    type: "problem",
+    docs: {
+      description:
+        "disallow print/debug statements like console.log, alert, etc.",
+    },
+    schema: [],
+    messages: {
+      noPrint: "Unexpected print statement: {{ name }}",
+    },
+  },
+  defaultOptions: [],
+  create(context): RuleListener {
+    return {
+      CallExpression(node) {
+        reportIfConsoleCall(node, context);
+        reportIfAlertCall(node, context);
+        reportIfDocumentWriteCall(node, context);
+      },
+    };
+  },
+});
+
+export default rule;
---
+++ test/rules/no-print.test.ts
@@ -0,0 +1,63 @@
+import { RuleTester } from "@typescript-eslint/rule-tester";
+import rule from "../../src/lib/rules/no-print";
+
+/**
+ * @fileoverview Tests for no-print.ts rule.
+ * @author b.ignited
+ */
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-print", rule, {
+  valid: [
+    // Test that a commented console.debug() gives no error
+    {
+      code: "console.debug()",
+    },
+    // Test that another object can call its own log method and it gives no error
+    {
+      code: 'someOtherObject.log("This is not console");',
+    },
+    // Test that accessing the alert method, but not calling it gives no error
+    {
+      code: "window.alert",
+    },
+    // Test that commented print statements give no error
+    {
+      code: '// console.log("This is a comment")',
+    },
+  ],
+
+  invalid: [
+    // Test that console.log() gives error
+    {
+      code: "console.log()",
+      errors: [{ messageId: "noPrint", data: { name: "console.log" } }],
+    },
+    // Test that console.info() gives error
+    {
+      code: "console.info()",
+      errors: [{ messageId: "noPrint", data: { name: "console.info" } }],
+    },
+    // Test that console.warn() gives error
+    {
+      code: "console.warn()",
+      errors: [{ messageId: "noPrint", data: { name: "console.warn" } }],
+    },
+    // Test that console.error() gives error
+    {
+      code: "console.error()",
+      errors: [{ messageId: "noPrint", data: { name: "console.error" } }],
+    },
+    // Test that alert() gives error
+    {
+      code: "alert()",
+      errors: [{ messageId: "noPrint", data: { name: "alert" } }],
+    },
+    // Test that document.write() gives error
+    {
+      code: "document.write()",
+      errors: [{ messageId: "noPrint", data: { name: "document.write" } }],
+    },
+  ],
+});
