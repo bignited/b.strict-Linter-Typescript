f35147e8d2094c7880e1653c07eba39ef1329916
Gepoverlow <lucas.stocker@bignited.be>
2025-09-18 14:54:53 +0200

    fixed tests - they now run against dist rules

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,20 +3,4 @@ module.exports = {
   preset: "ts-jest/presets/default-esm",
   testMatch: ["**/test/**/*.test.ts"],
   testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", { useESM: true }],
-    "^.+\\.js$": ["ts-jest", { useESM: true }],
-  },
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
-  },
-  testPathIgnorePatterns: [
-    "/.history/",
-    ...require("jest-config").defaults.testPathIgnorePatterns,
-  ],
-  transformIgnorePatterns: ["/node_modules/"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
